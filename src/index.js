e98b3320ca1fe9190d2b1125f8848d7bea3ea664
Foad Botan <foadbotan@gmail.com>
2022-09-05 21:35:13 +1000

    Created styles.css and added resets

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import * as ReactDOM from "react-dom/client";
 import App from "./App";
+import "./styles.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
