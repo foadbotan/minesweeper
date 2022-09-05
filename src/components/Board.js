e7558393dfa1abd1289e70feb379e349052a1c39
Foad Botan <foadbotan@gmail.com>
2022-09-05 22:21:40 +1000

    Create board state, fill and style board

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,9 +1,28 @@
+import { useState } from "react";
 import Tile from "./Tile";
 
+const BOARD_SIZE = 5;
+const MINE_COUNT = 5;
+
 export default function Board() {
-  return (
-    <div className="board">
-      <Tile />
-    </div>
-  );
+  const [board, setBoard] = useState(newBoard());
+
+  console.log(board);
+  return <div className="board">{board.map(Tile)}</div>;
+}
+
+function newBoard() {
+  let board = [];
+
+  for (let x = 0; x < BOARD_SIZE; x++) {
+    for (let y = 0; y < BOARD_SIZE; y++) {
+      board.push({
+        y,
+        x,
+        value: 3,
+      });
+    }
+  }
+
+  return board;
 }
