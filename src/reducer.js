80f6752d970896317eeaf4fe12409d8373f56062
Foad Botan <foadbotan@gmail.com>
2022-09-20 12:35:00 +1000

    Fixed: removed refs to old board state

--- src/reducer.js
+++ src/reducer.js
@@ -3,24 +3,20 @@ export const ACTIONS = {
   FLAG: "flag",
 };
 
-export default function reducer(state, action) {
+export default function reducer(board, action) {
   const tileId = action.payload;
 
   switch (action.type) {
     case ACTIONS.SHOW:
-      return {
-        board: state.board.map((tile) => {
-          if (tile.id !== tileId || tile.flag) return tile;
-          return { ...tile, show: true };
-        }),
-      };
+      return board.map((tile) => {
+        if (tile.id !== tileId || tile.flag) return tile;
+        return { ...tile, show: true };
+      });
     case ACTIONS.FLAG:
-      return {
-        board: state.board.map((tile) => {
-          if (tile.id !== tileId || tile.show) return tile;
-          return { ...tile, flag: !tile.flag };
-        }),
-      };
+      return board.map((tile) => {
+        if (tile.id !== tileId || tile.show) return tile;
+        return { ...tile, flag: !tile.flag };
+      });
   }
 
   return state;
