e5b03fb23f90c06ff5f60f03b39d7c50aea7db3d
Foad Botan <foadbotan@gmail.com>
2022-09-25 21:54:01 +1000

    Fixed: restored normal mine count on Expert

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import createNewBoard from "./utils/createNewBoard";
 const BOARD_TYPE = {
   BEGINNER: { boardWidth: 8, boardHeight: 8, mineCount: 10 },
   INTERMEDIATE: { boardWidth: 16, boardHeight: 16, mineCount: 40 },
-  EXPERT: { boardWidth: 30, boardHeight: 16, mineCount: 50 },
+  EXPERT: { boardWidth: 30, boardHeight: 16, mineCount: 99 },
 };
 
 export default function App() {
